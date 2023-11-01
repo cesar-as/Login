f42cd7f2643a34f050d25054ca5dabb6de18d917
cesar-as <csilvaatc@gmail.com>
2023-11-01 06:07:08 -0300

    Login layout improvement

--- Login/ContentView.swift
+++ Login/ContentView.swift
@@ -13,39 +13,80 @@ struct ContentView: View {
     @State var username: String = ""
     @State var password: String = ""
     
+    let backgroundColor = Color(red: 33/255, green: 160/255, blue: 160/255)
+    let cardBackgroundColor = Color(red: 35/255, green: 43/255, blue: 68/255)
+    let buttonColor = Color(red: 30/255, green: 243/255, blue: 223/255)
+    let textFieldColor = Color(red: 72/255, green: 79/255, blue: 102/255)
+    let imageColor = Color(red: 147/255, green: 154/255, blue: 179/255)
+    
     var body: some View {
         ZStack {
-            Color.teal.edgesIgnoringSafeArea(.all)
+            backgroundColor.edgesIgnoringSafeArea(.all)
             VStack {
                 Text("SIGN IN")
+                    .padding(10)
                     .font(.system(size: 24))
-                    .padding(EdgeInsets(top: 20, leading: 40, bottom: 20, trailing: 40))
+                    .background(buttonColor)
                 Image(systemName: "person.crop.circle")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .foregroundColor(.gray)
+                    .foregroundColor(imageColor)
                     .frame(width: 100, height: 100)
                     .padding(10)
-                TextField("username", text: $username)
-                    .textFieldStyle(.roundedBorder)
-                    .foregroundColor(.black)
-                    .keyboardType(.emailAddress)
-                    .padding(10)
-                SecureField("password", text: $password)
-                    .textFieldStyle(.roundedBorder)
-                    .foregroundColor(.black)
-                    .keyboardType(.emailAddress)
-                    .padding(10)
+                
+                HStack {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .foregroundColor(imageColor)
+                        .padding(.leading, 10)
+                    TextField("username", text: $username)
+                        .frame(height: 30)
+                        .textFieldStyle(.plain)
+                        .foregroundColor(imageColor)
+                        .background(textFieldColor)
+                        .keyboardType(.emailAddress)
+                        .padding(.trailing, 10)
+                }
+                .frame(height: 35)
+                .background(textFieldColor)
+                .cornerRadius(8)
+                .padding(.horizontal, 10)
+                
+                HStack {
+                    Image(systemName: "lock.circle")
+                        .resizable()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .foregroundColor(imageColor)
+                        .padding(.leading, 10)
+                    SecureField("password", text: $password)
+                        .frame(height: 30)
+                        .textFieldStyle(.plain)
+                        .foregroundColor(imageColor)
+                        .background(textFieldColor)
+                        .keyboardType(.emailAddress)
+                        .padding(10)
+                }
+                .frame(height: 35)
+                .background(textFieldColor)
+                .cornerRadius(8)
+                .padding([.horizontal, .top], 10)
                 
                 HStack{
-                    Text("Remember-me")
-                        .font(.system(size: 12))
-                        .foregroundColor(.teal)
-                    Spacer()
-                    ZStack{
-                        Text("Forgot your password?")
-                            .font(.system(size: 12))
+                    Button {
+                        print("Remember!!")
+                    } label: {
+                        Text("Remember-me")
                             .foregroundColor(.teal)
+                            .font(.system(size: 13))
+                    }
+                    Spacer()
+                    Button {
+                        print("Forgot!!")
+                    } label: {
+                        Text("Forgot password?")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 13))
                     }
                 }
                 .padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 10))
@@ -54,17 +95,24 @@ struct ContentView: View {
                     print("Logar")
                 } label: {
                     Text("LOGIN")
-                        .foregroundColor(Color.white)
+                        .foregroundColor(Color.black)
+                        .fontWeight(.semibold)
                         .frame(height: 50)
                         .frame(maxWidth: .infinity)
-                        .background(Color.teal)
+                        .background(buttonColor)
                 }
                 .cornerRadius(8)
                 .padding(10)
-
-                
+                Button {
+                    print("New Account!!")
+                } label: {
+                    Text("Create new account")
+                        .foregroundColor(.teal)
+                        .font(.system(size: 14))
+                        .padding(.bottom)
+                }
             }
-            .background(Color.white)
+            .background(cardBackgroundColor)
             .padding(EdgeInsets(top: 120, leading: 40, bottom: 200, trailing: 40))
         }
     }
